3774e4650dd6ba306a866ada676a077c3bb978af
Alexandre Freire García <xandrefreire@gmail.com>
2017-04-17 22:54:44 +0200

    more tests

--- String Calculator Kata/Calculator.swift
+++ String Calculator Kata/Calculator.swift
@@ -12,9 +12,18 @@ final class Calculator {
     
     func add(numbers: String) -> Int {
         guard numbers.characters.count != 0 else { return 0 }
-        let array = numbers.components(separatedBy: ",").flatMap{ Int($0) }
+        let delimeters = CharacterSet([",", "\n", detectCustomDelimiter(nums: numbers)])
+        let array = numbers.components(separatedBy: delimeters).flatMap{ Int($0) }
         
         return array.reduce(0, +)
+        
+    }
+    
+    private func detectCustomDelimiter(nums: String) -> UnicodeScalar {
+        guard let match = nums.range(of: "(?<=//)[^\n]+", options: .regularExpression) else {
+            return UnicodeScalar("x")!
+        }
+        return UnicodeScalar(nums.substring(with: match))!
         
     }
 }
--- String Calculator KataTests/String_Calculator_KataTests.swift
+++ String Calculator KataTests/String_Calculator_KataTests.swift
@@ -36,4 +36,16 @@ class String_Calculator_KataTests: XCTestCase {
     func testAddUnknownAmountOfNumbers() {
         XCTAssertEqual(15, calculator.add(numbers: "1,3,2,6,3"))
     }
+    
+    func testAddNewLineDelimiter() {
+        XCTAssertEqual(4, calculator.add(numbers: "1\n3"))
+        XCTAssertEqual(12, calculator.add(numbers: "1,3,2\n6"))
+    }
+    
+    func testAddWithCustomDelimiter() {
+        XCTAssertEqual(3, calculator.add(numbers: "//;\n1;2"))
+        XCTAssertEqual(6, calculator.add(numbers: "//;\n1;2,3"))
+        XCTAssertEqual(7, calculator.add(numbers: "//;\n1;2,3\n1"))
+        
+    }
 }
