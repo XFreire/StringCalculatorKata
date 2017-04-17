51cdc3bcdd76745c0ac95caaf3369445a90cbf31
Alexandre Freire García <xandrefreire@gmail.com>
2017-04-17 22:03:35 +0200

    First commit

---
+++ String Calculator Kata/Calculator.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator.swift
+//  String Calculator Kata
+//
+//  Created by Alexandre Freire García on 17/4/17.
+//  Copyright © 2017 Alexandre Freire García. All rights reserved.
+//
+
+import Foundation
