063d4c05803d0da072ef15e5db3408a3dc40460f
Alexandre Freire García <xandrefreire@gmail.com>
2017-04-20 15:17:43 +0200

    negative values test added

--- String Calculator Kata/Calculator.swift
+++ String Calculator Kata/Calculator.swift
@@ -8,12 +8,18 @@
 
 import Foundation
 
+enum CalculatorError: Error {
+    case negativeValue
+}
+
 final class Calculator {
     
-    func add(numbers: String) -> Int {
+    func add(numbers: String) throws -> Int {
         guard numbers.characters.count != 0 else { return 0 }
         let delimeters = CharacterSet([",", "\n", detectCustomDelimiter(nums: numbers)])
         let array = numbers.components(separatedBy: delimeters).flatMap{ Int($0) }
+        
+        if thereAreNegativeValues(in: array) { throw CalculatorError.negativeValue }
         
         return array.reduce(0, +)
         
@@ -25,5 +31,9 @@ final class Calculator {
         }
         return UnicodeScalar(nums.substring(with: match))!
         
+    }
+    
+    private func thereAreNegativeValues(in array: [Int]) -> Bool {
+        return (array.filter{ $0 < 0 }).count > 0
     }
 }
--- String Calculator KataTests/String_Calculator_KataTests.swift
+++ String Calculator KataTests/String_Calculator_KataTests.swift
@@ -22,30 +22,36 @@ class String_Calculator_KataTests: XCTestCase {
     }
     
     func testAddEmptyString() {
-        XCTAssertEqual(0, calculator.add(numbers: ""))
+        XCTAssertEqual(0, try! calculator.add(numbers: ""))
     }
     
     func testAddOneNumber() {
-        XCTAssertEqual(1, calculator.add(numbers: "1"))
+        XCTAssertEqual(1, try! calculator.add(numbers: "1"))
     }
     
     func testAddTwoNumbers() {
-        XCTAssertEqual(4, calculator.add(numbers: "1,3"))
+        XCTAssertEqual(4, try! calculator.add(numbers: "1,3"))
     }
     
     func testAddUnknownAmountOfNumbers() {
-        XCTAssertEqual(15, calculator.add(numbers: "1,3,2,6,3"))
+        XCTAssertEqual(15, try! calculator.add(numbers: "1,3,2,6,3"))
     }
     
     func testAddNewLineDelimiter() {
-        XCTAssertEqual(4, calculator.add(numbers: "1\n3"))
-        XCTAssertEqual(12, calculator.add(numbers: "1,3,2\n6"))
+        XCTAssertEqual(4, try! calculator.add(numbers: "1\n3"))
+        XCTAssertEqual(12, try! calculator.add(numbers: "1,3,2\n6"))
     }
     
     func testAddWithCustomDelimiter() {
-        XCTAssertEqual(3, calculator.add(numbers: "//;\n1;2"))
-        XCTAssertEqual(6, calculator.add(numbers: "//;\n1;2,3"))
-        XCTAssertEqual(7, calculator.add(numbers: "//;\n1;2,3\n1"))
-        
+        XCTAssertEqual(3, try! calculator.add(numbers: "//;\n1;2"))
+        XCTAssertEqual(6, try! calculator.add(numbers: "//;\n1;2,3"))
+        XCTAssertEqual(7, try! calculator.add(numbers: "//;\n1;2,3\n1"))
     }
+    
+    func testAddWithNegativeValuesThrowsException() {
+        XCTAssertThrowsError(try calculator.add(numbers: "1,-5")) { error in
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.negativeValue)
+        }
+    }
+    
 }
